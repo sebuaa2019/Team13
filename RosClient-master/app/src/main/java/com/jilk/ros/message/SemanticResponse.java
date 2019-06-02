24bd392026470bf07830d4ce594df2d6a1261e3f
高则晗 <15271124@buaa.edu.cn>
2019-06-02 23:03:37 +0800

    documents and maintance directory tree

--- RosClient-master/app/src/main/java/com/jilk/ros/message/SemanticResponse.java
+++
@@ -1,9 +0,0 @@
-package com.jilk.ros.message;
-
-/**
- * Created by xxhong on 16-11-17.
- */
-@MessageType(string = "std_msgs/String")
-public class SemanticResponse extends Message {
-    public String result;
-}
